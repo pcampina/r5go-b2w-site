e84ccc68180f4233390765acf25ebe0ab2d11f4a
pcampina <pablo.dsn@gmail.com>
2020-05-04 12:18:46 -0300

    change content

--- src/App.js
+++ src/App.js
@@ -29,21 +29,6 @@ function App() {
           quantidade e clicar em confirmar e go! No mesmo dia entregue na sua
           casa.
         </p>
-
-        <h3>O que é Go Scan?</h3>
-
-        <p>
-          Tecnologia de leitura de código de barras para compra de produtos
-          disponibilizados nas lojas de conveniência para entrega no mesmo dia.
-          Buscando atuar inclusive na compra reativa, ou seja, naquela compra
-          que o cliente só lembra de comprar quando acaba ou quando ele tem
-          desejo de consumir ou comprar algo que não tem em casa como, por
-          exemplo, chocolates, café, folha A4. Qualquer produto cadastrado na
-          base de dados com código de barras será fácil, prático e sem muito
-          segredo para comprar. Depois lançado todos os produtos só decidir a
-          quantidade e clicar em confirmar e go! No mesmo dia entregue na sua
-          casa.
-        </p>
       </Section>
 
       <Section img={AiImage} title="GO Bot" id="gobot" reverse>
@@ -57,15 +42,6 @@ function App() {
         <p>
           Foram produtos desenvolvidos na maratona de tecnologia (Mega Hack)
           para atender o Grupo B2W.
-        </p>
-
-        <h3>O que é Go Bot?</h3>
-
-        <p>
-          Chatbot para o atendimento ao consumidor com automação e inteligência
-          artificial que interpreta o que o cliente digita ou diz sendo canal
-          direto a qualquer hora para esclarecer dúvidas, dar informações e
-          solucionar problemas com a compra e/ou pós venda.
         </p>
       </Section>
 
