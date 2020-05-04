449dba713d1764ec7e9f9b89e2f3d59da7c73416
pcampina <pablo.dsn@gmail.com>
2020-05-04 13:30:03 -0300

    change vision

--- src/MissionVision/index.js
+++ src/MissionVision/index.js
@@ -18,10 +18,15 @@ function MissionVision({ id }) {
 
           <div className="content">
             <h3>Visão</h3>
+            
+            <p>Ser referência em soluções tecnológicas para multinacionais do segmento do e-commerce, resolvendo os gaps de processos que essas instituições têm na atuação com seus clientes.</p>
 
-            <p>Ser referência no segmento de soluções de tecnológicas para multinacionais do segmento do e-commerce resolvendo os gaps de processos que essas instituições têm na atuação com seus clientes.</p>
             <p>Upgrade das soluções para versão Beta.</p>
-            <p>Inclusões de inteligência artificial e big data com objetivo de ser assertivo na forma de atuar com o público reativo (são o público que compra de última hora, já que compra no ato da finalização do produto que consumiram) que nossos clientes atendem. Essas inclusões nos permite traçar um perfil previsível de compra desse público para que sejam avisados antes quando seus produtos estiverem para acabar e assim comprarem antes de finalizar aumentando a eficiência da compra, bem como facilitando a logística de entrega (não ter falta de estoque desses produtos consumidos nas lojas de conveniência).</p>
+
+            <p>
+              Inclusões de inteligência artificial e big data com objetivo de ser assertivo na atuação com o público reativo (são o público que compra de última hora, já que compra no ato da finalização do produto que consumiram e que nossos clientes atendem).
+              A I.A permitirá traçar um perfil previsível de compra desse público para que sejam avisados com antecedência, quando seus produtos estiverem para acabar. E assim comprarem antes de finalizar, aumentando a eficiência da compra, bem como facilitando a logística de entrega (de não ter falta de estoque desses produtos consumidos nas lojas de conveniência para entregar).
+            </p>
           </div>
         </div>
       </div>
